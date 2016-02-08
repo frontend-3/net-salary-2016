925bad81245d4b2b6836938ab605d6f52bc139b5
Elizabeth Manrique <ecmanrique@gmail.com>
2016-02-08 17:54:33 -0500

    fix(salary): change dom elements

--- src/app/salary/scripts/salary.component.ts
+++ src/app/salary/scripts/salary.component.ts
@@ -13,22 +13,25 @@ export class SalaryComponent implements OnInit {
   public salary               = new Salary();
   public events               : {};
   private _catchDom           : () => void;
+  private _afterCatchDom      : () => void;
   private _suscribeEvents     : () => void;
   private _initialize         : () => void;
   private _numOfQuestions     : number;
   private _numCurrentQuestion : number;
-  private _currentQuestion    : HTMLElement;
   private _fn                 : {
     updateProgressBar         : () => void
   };
   private _dom                : {
-    progressBar : HTMLElement,
-    form        : HTMLElement
+    progressBar     : HTMLElement,
+    form            : HTMLElement,
+    questionItems   : any,
+    currentQuestion : HTMLElement,
+    nextQuestion    : HTMLElement
   } = <any> {};
-  private _settings           : {
+  private _settings            : {
     progressBar         : string,
-    questionItem        : string,
-    questionItemActive  : string,
+    questionItems       : string,
+    currentQuestion     : string,
     form                : string,
     transitionEventName : string
   };
@@ -43,39 +46,49 @@ export class SalaryComponent implements OnInit {
   initModule() {
     this._settings = {
       progressBar         : '.ProgressBar',
-      questionItem        : '.QuestionList-item',
-      questionItemActive  : '.QuestionList-item.is-active',
+      questionItems       : '.QuestionList-item',
+      currentQuestion     : '.QuestionList-item.is-active',
       form                : '.SalaryForm',
       transitionEventName : 'webkitTransitionEnd'
     };
 
     this._catchDom = () => {
-      console.log(this._dom);
-      this._dom = {
-        progressBar : (<HTMLElement> document.querySelector(this._settings.progressBar)),
-        form        : (<HTMLElement> document.querySelector(this._settings.form))
-      };
+      this._dom.progressBar     =
+        (<HTMLElement> document.querySelector(this._settings.progressBar));
+      this._dom.form            =
+        (<HTMLElement> document.querySelector(this._settings.form));
+      this._dom.questionItems   =
+        document.querySelectorAll(this._settings.questionItems);
+      this._dom.currentQuestion =
+        (<HTMLElement> document.querySelector(this._settings.currentQuestion));
+      this._dom.nextQuestion    =
+      (<HTMLElement> this._dom.currentQuestion.nextElementSibling);
+    };
+
+    this._afterCatchDom = () => {
+      this._numOfQuestions      = this._dom.questionItems.length;
+      this._numCurrentQuestion  = parseInt(this._routeParams.get('id'));
     };
 
     this._suscribeEvents = () => {
       this._dom.progressBar.addEventListener(
           this._settings.transitionEventName,
-          () => this._dom.form.classList.remove('is-change')
+          () => {
+            let nextId;
+
+            nextId = this._numCurrentQuestion + 1;
+            this._dom.form.classList.remove('is-change');
+            this._router.navigate( ['Questions', { id: nextId }] );
+          }
       );
     };
 
     this.events = {
       onNextQuestion: () => {
-        let nextQuestion;
-
-        this._currentQuestion =  (<HTMLElement> document.querySelector(
-                                                          this._settings.
-                                                          questionItemActive));
-        nextQuestion          = (<HTMLElement> this._currentQuestion.nextElementSibling);
         this._fn.updateProgressBar();
         this._dom.form.classList.add('is-change');
-        this._currentQuestion.classList.remove('is-active');
-        nextQuestion.classList.add('is-active');
+        this._dom.currentQuestion.classList.remove('is-active');
+        this._dom.nextQuestion.classList.add('is-active');
       },
       onEndTransitionFn: () => {
         this._dom.form.classList.remove('is-change');
@@ -91,12 +104,10 @@ export class SalaryComponent implements OnInit {
     };
 
     this._initialize = () => {
-      this._numOfQuestions      = (document.querySelectorAll(
-                                                this._settings.questionItem
-                                                )).length;
-      this._numCurrentQuestion  = parseInt(this._routeParams.get('id'));
       this._catchDom();
+      this._afterCatchDom();
       this._suscribeEvents();
+      console.log(this._routeParams.get('id'));
     };
   }
 
